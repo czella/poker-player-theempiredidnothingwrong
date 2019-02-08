3d9acfa57e181947cfc181df4b8a5faff7b71785
Laszlo Czellaho <laszlo.czellaho@gmail.com>
2019-02-08 14:48:21 +0100

    Update test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import player
 import json
 
-testJSon = json.loads("""{"community_cards": [],
+testJSon_one_high = json.loads("""{"community_cards": [],
 "minimum_raise": 2, 
 "big_blind": 4, 
 "orbits": 0, 
@@ -36,6 +36,78 @@ testJSon = json.loads("""{"community_cards": [],
 "tournament_id": "5c38a553b0fea40004000003"}
     """)
 
+testJSon_two_high = json.loads("""{"community_cards": [],
+"minimum_raise": 2, 
+"big_blind": 4, 
+"orbits": 0, 
+"in_action": 3, 
+"bet_index": 2, 
+"current_buy_in": 4, 
+"round": 0, 
+"players": [{"id": 0, "bet": 0, "version": "Pony 1.0.0", "time_used": 0, "stack": 1000, "status": "out", "name": "Bright Pony"}, 
+            {"id": 1, "bet": 2, "version": "1.0", "time_used": 0, "stack": 998, "status": "active", "name": "PokerMasters"}, 
+            {"id": 2, "bet": 4, "version": "ERROR: Unreachable", "time_used": 0, "stack": 996, "status": "active", "name": "NADagascar"}, 
+            {"hole_cards": [{"suit": "hearts", "rank": "J"}, 
+                            {"suit": "diamonds", "rank": "Q"}], 
+              "bet": 0, 
+              "version": "DS.1.0.0", 
+              "time_used": 0, 
+              "id": 3, 
+              "stack": 1000, 
+              "status": "active", 
+              "name": "TheEmpireDidNothingWrong"}, 
+            {"id": 4, 
+            "bet": 0, 
+            "version": "1.0", 
+            "time_used": 0, 
+            "stack": 1000, 
+            "status": "active", 
+            "name": "Hive"}, 
+            {"id": 5, "bet": 0, "version": "Gopnik_FM_ver_1.0", "time_used": 0, "stack": 1000, "status": "active", "name": "Gopnik FM"}], 
+"small_blind": 2, 
+"game_id": "5c5d4b96a972e80004000021", 
+"dealer": 0, 
+"pot": 6, 
+"tournament_id": "5c38a553b0fea40004000003"}
+    """)
+
+testJSon_three_out = json.loads("""{"community_cards": [],
+"minimum_raise": 2, 
+"big_blind": 4, 
+"orbits": 0, 
+"in_action": 3, 
+"bet_index": 2, 
+"current_buy_in": 4, 
+"round": 0, 
+"players": [{"id": 0, "bet": 0, "version": "Pony 1.0.0", "time_used": 0, "stack": 1000, "status": "out", "name": "Bright Pony"}, 
+            {"id": 1, "bet": 2, "version": "1.0", "time_used": 0, "stack": 998, "status": "out", "name": "PokerMasters"}, 
+            {"id": 2, "bet": 4, "version": "ERROR: Unreachable", "time_used": 0, "stack": 996, "status": "out", "name": "NADagascar"}, 
+            {"hole_cards": [{"suit": "hearts", "rank": "J"}, 
+                            {"suit": "diamonds", "rank": "Q"}], 
+              "bet": 0, 
+              "version": "DS.1.0.0", 
+              "time_used": 0, 
+              "id": 3, 
+              "stack": 1000, 
+              "status": "active", 
+              "name": "TheEmpireDidNothingWrong"}, 
+            {"id": 4, 
+            "bet": 0, 
+            "version": "1.0", 
+            "time_used": 0, 
+            "stack": 1000, 
+            "status": "active", 
+            "name": "Hive"}, 
+            {"id": 5, "bet": 0, "version": "Gopnik_FM_ver_1.0", "time_used": 0, "stack": 1000, "status": "active", "name": "Gopnik FM"}], 
+"small_blind": 2, 
+"game_id": "5c5d4b96a972e80004000021", 
+"dealer": 0, 
+"pot": 6, 
+"tournament_id": "5c38a553b0fea40004000003"}
+    """)
+
 if __name__ == "__main__":
     player = player.Player()
-    print(player.betRequest(testJSon))
+    print(player.betRequest(testJSon_one_high))
+    print(player.betRequest(testJSon_two_high))
+    print(player.betRequest(testJSon_three_out))
